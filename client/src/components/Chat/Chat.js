80babe7eb1503f4d91d675bc5eb8fdf48911f3d3
dougdboliveira <douglasdboliveira@gmail.com>
2021-02-22 17:30:47 -0300

    start connecting client to server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+var connectionOptions =  {
+    "force new connection" : true,
+    "reconnectionAttempts": "Infinity", 
+    "timeout" : 10000,                  
+    "transports" : ["websocket"]
+};
+
+/*
+ * queryString é um objeto importado do módulo de mesmo nome
+ * queryString.parse() é um método que retorna um objeto a partir de uma string
+ * Isso é possível porque ele usa um formato específico para extrair os atributos
+ */
+// location é um objeto e search é um atributo
+// location vem de 'Router'
+
+let socket;
+
+const Chat = ( props ) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+    const { location } = props;
+    // same as: const location = props.location; (from JS)
+
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+       const { name, room } = queryString.parse(location.search);
+
+       socket = io.connect(ENDPOINT,connectionOptions);
+
+       setName(name);
+       setRoom(room);
+
+       console.log(socket);
+    });
+
     return (
         <h1>Chat</h1>
     )
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -14,7 +14,7 @@ const Join = () => {
                 <h1 className="heading">Hello Walrus</h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div>
                 <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}/></div>
-                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room${room}`}>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                     <button className="button mt-20" type="submit">Sign In</button>
                 </Link>
             </div>

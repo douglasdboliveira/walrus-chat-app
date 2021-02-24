b6d23c9c7b9cdabcf647721eb8a9889e4a58b404
dougdboliveira <douglasdboliveira@gmail.com>
2021-02-24 12:34:21 -0300

    it works!

--- server/index.js
+++ server/index.js
@@ -23,6 +23,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.rooms) });
+
         callback();
     });
 
@@ -30,6 +32,7 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.rooms) });
 
         callback();
     })

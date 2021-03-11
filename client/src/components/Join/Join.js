a20f7132d7c18db5b8f31a9d75ed6f5728aee20a
dougdboliveira <douglasdboliveira@gmail.com>
2021-03-11 14:30:04 -0300

    start creating rooms

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Join.css';
@@ -6,6 +6,7 @@ import './Join.css';
 const Join = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const dropdownRef = useRef('');
 
     return (
         <div className="joinOuterContainer">
@@ -13,8 +14,17 @@ const Join = () => {
             <img src="Logo.png" alt="" width="81" height="93" />
                 <h1 className="heading">Hello Walrus</h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div>
-                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}/></div>
-                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                {/*<div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}/></div>*/}
+                <div>
+                    <select className="dropdown" ref={dropdownRef} onClick={(event) => setRoom(event.target.value)}>   
+                        <option>Select your room</option>   
+                        <option>feijoada</option>   
+                        <option>tapioca</option>   
+                        <option>brigadeiro</option>   
+                        <option>farofa</option>
+                    </select>
+                </div>
+                <Link onClick={event => (!name || room == 'Select your room') ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                     <button className="button mt-20" type="submit">Sign In</button>
                 </Link>
             </div>

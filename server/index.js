fbd8341c1e1043fa98ef900e2e680a204387baa3
dougdboliveira <douglasdboliveira@gmail.com>
2021-03-24 16:10:03 -0300

    keeping it simple

--- server/index.js
+++ server/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const { addUser, verifyExistingRoom, removeUser, getUser } = require('./users.js');
-const { addRooms, addMessages, removeRooms, readMessages, getRooms, getLastMessages } = require('./rooms.js');
-
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -14,70 +11,18 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    socket.on('join', ({ name, room }, callback) => {
-        // Um ou outro
-        const { error, user } = addUser({ id: socket.id, name, room }); 
+    socket.on('join', ({ name, room }) => {
+        socket.emit('message', { user: 'admin', text: `${name}, welcome to the room ${room}`, room }); // Emitir mensagem ao admin
+        socket.broadcast.to(room).emit('message', { user: 'admin', text: `${name} has joined!`, room }); // Emitir mensagem aos outros usuários
 
-        if(error) return callback(error);
+        socket.join(room);
+    });
 
-        const addedRoom = addRooms(room);
-        console.log(addedRoom);
-
-        io.emit('rooms', getRooms());
-        io.emit('lastMessages', getLastMessages());
-
-        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` }); // Emitir mensagem ao admin
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` }); // Emitir mensagem aos outros usuários
-
-        socket.join(user.room);
+    socket.on('sendMessage', (user, room, message, callback) => {
+        io.emit('message', { user, room, text: message });
 
         callback();
     });
-
-    socket.on('readMessages', () => {
-        const user = getUser(socket.id);
-        readMessages(user); // Tentar enviar o usuário completo - OK
-    });
-
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-
-        console.log(user);
-
-        io.to(user.room).emit('message', { user: user.name, text: message });
-        const rooms = addMessages(message, user.room, user.name); 
-
-        rooms.forEach((room) => {
-            console.log(room);
-        })
-        
-        io.emit('lastMessages', getLastMessages());
-
-        callback();
-    });
-
-    socket.on('leaveRoom', () => {
-        socket.disconnect(true);
-    });
-
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
-
-        const searchResult = verifyExistingRoom(user.room);
-        // Pra ver se ainda tem usuários na sala
-
-        const deletedRoom = removeRooms(searchResult); 
-        // ou o nome da sala, ou undefined
-
-        console.log(`Deleted room: ${deletedRoom}`);
-        // Se nenhuma sala for deletada, vai ser "Deleted room: undefined"
-
-        if(user) {
-            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` });
-        }
-
-        io.emit('rooms', getRooms());
-    })
 })
 
 app.use(router);
